5c02763be90b6d1d73a0fbfc9be76d4fa9780cc7
miomaeshima <miomamomo@gmail.com>
2021-01-11 21:26:33 +0900

    edited Spotify.tsx for autofill

--- src/components/Feed/Spotify.tsx
+++ src/components/Feed/Spotify.tsx
@@ -3,10 +3,11 @@ import React from 'react'
 function Spotify(){
     
     //obtain access token 
-       const access_token = window.location.search.slice(14);
+     const access_token = window.location.search.slice(14);
+     const enterUri : any = document.getElementById("enterUri")
 
      function sendUri() {
-        const enterUri : any = document.getElementById("enterUri") 
+        //const enterUri : any = document.getElementById("enterUri") 
         const uri = enterUri.value
         const body = {uri: uri}
         console.log(body)
@@ -16,29 +17,16 @@ function Spotify(){
         body:JSON.stringify(body)})        
     };
     
-   const enterUri : any = document.getElementById("enterUri")
 
    function autoFill(){
-      // const enterUri : any = document.getElementById("enterUri")
-       let autoUri = getUri()
-
-       enterUri.value = autoUri 
-   }
-
- 
-
-  function getUri () {
     if(access_token){
-    console.log(access_token);
-    fetch("https://api.spotify.com/v1/me/player?additional_types=episode", {
+      fetch("https://api.spotify.com/v1/me/player?additional_types=episode", {
       headers: { Authorization: "Bearer " + access_token },
     })
     .then((response) => response.json())
     .then((data) => {
-      const body = { uri: data.item.uri };
-      console.log(body);
-      return body
-      
+      const uri = data.item.uri;      
+      enterUri.value = uri      
     })
     .catch((err)=>(console.log("No uri now.")));
   } else {
