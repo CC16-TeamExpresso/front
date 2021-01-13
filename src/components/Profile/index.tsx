4837eecdeefd049bb7f6cfc60751ef06f85eaec1
Francis <gan_fu_rui@hotmail.com>
2021-01-13 16:35:29 +0900

    css styling for Profile component

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -8,11 +8,14 @@ export default function Profile() {
 
 	return (
 		<div className='App'>
+			<p>Post History</p>
 			<div className='history-feed'>
 
 
 			</div>
+			<div className='profile-info-box'>
 
+			</div>
 		</div>
 	);
 }
