a0b5837632b8fd5e4d5c532a2aada5a8efd3c29a
Soshi <SoshiK@users.noreply.github.com>
2021-01-19 17:15:51 +0900

    added another user page

---
+++ src/components/User/index.tsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import Post from '../Feed/Post';
+import './user.css';
+
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:8050";
+
+export default function User(props: any) {
+  const [user, setUser] = useState("");
+  const [feedUsers, setFeedUsers] = useState([]);
+  const [follow,setFollow] = useState("Follow");
+  const [isFollow, setIsFollow] = useState(true);
+  const { params } = props.match;
+  const userid = params.userid;
+  const history = useHistory();
+  useEffect(() => {
+    fetch(BACKEND_URL + "/api/user/" + userid, {
+      method:"GET",
+      headers: {
+        'Content-Type': 'application/json',
+        'token': localStorage.getItem('token') || ''
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+        setUser(data.result.username);
+        setFeedUsers(data.result.posts);
+        if (data.result.follow) {
+          setFollow("Following");
+        } else {
+          setIsFollow(false);
+        }
+      })
+  },[])
+
+  const followUser = () => {
+    if (isFollow) {
+      alert("You've already followed this user!");
+    } else {
+      fetch(BACKEND_URL + '/api/follow/' + userid,{
+        method: "PATCH",
+        headers: {
+          'Content-Type': 'application/json',
+          'token': localStorage.getItem('token') || ''
+        }
+      })
+        .then(res => res.json())
+        .then(data => {
+          if(data.status === "ok") {
+            setIsFollow(true);
+            setFollow("Following");
+          } else {
+            alert('Something wrong. Please try again.');
+          }
+        })
+    }
+  }
+  return (
+		<div className='App'>
+      <button className='goback-button' onClick={() => history.goBack()}>Go Back</button>		
+      <h1 className='posthistory-title'>{user}'s post history</h1>
+      <button className='follow-button' onClick={followUser}>{follow}</button>
+      <div className='history-feed'>
+        {feedUsers.map((feedUser:any) => {
+          return <Post isHistory={true} username={user} uri={feedUser.uri} id={feedUser._id} like={feedUser.like} />
+        })}	
+      </div>
+      <div className='profile-info-box'>
+      </div>
+		</div>
+	);
+}
